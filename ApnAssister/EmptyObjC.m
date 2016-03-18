6401f7df858beea7b2898b1c8b1bdc75bfe99cc4
WataruSuzuki <wataru0406@gmail.com>
2016-03-18 12:04:14 +0900

    Objective-CとSwiftのBridging-Headerを生成

---
+++ ApnAssister/EmptyObjC.m
@@ -0,0 +1,9 @@
+//
+//  EmptyObjC.m
+//  ApnAssister
+//
+//  Created by WataruSuzuki on 2016/03/18.
+//  Copyright © 2016年 WataruSuzuki. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>

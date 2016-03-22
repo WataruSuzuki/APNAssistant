96c594ddb055623cf65108295457c81d309cf1b0
WataruSuzuki <wataru0406@gmail.com>
2016-03-22 11:24:25 +0900

    必要なViewControllerの箱の追加＆不要なファイルの削除

--- ApnAssister/EmptyObjC.m
+++
@@ -1,9 +0,0 @@
-//
-//  EmptyObjC.m
-//  ApnAssister
-//
-//  Created by WataruSuzuki on 2016/03/18.
-//  Copyright © 2016年 WataruSuzuki. All rights reserved.
-//
-
-#import <Foundation/Foundation.h>

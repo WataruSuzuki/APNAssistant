fadf54957862a9ac1ca47dc21808dbb67e705a1e
WataruSuzuki <wataru0406@gmail.com>
2016-03-24 20:03:29 +0900

    カスタムTableViewCell向けにファイルを追加（中身はこれから）

---
+++ ApnAssister/Views/TextFieldCell.swift
@@ -0,0 +1,24 @@
+//
+//  TextFieldCell.swift
+//  ApnAssister
+//
+//  Created by WataruSuzuki on 2016/03/24.
+//  Copyright © 2016年 WataruSuzuki. All rights reserved.
+//
+
+import UIKit
+
+class TextFieldCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

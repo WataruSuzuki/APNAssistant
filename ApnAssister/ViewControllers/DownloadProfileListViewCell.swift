24ab1c7b008104a02335878b012a07adb0d2501f
WataruSuzuki <wataru0406@gmail.com>
2016-04-05 20:07:00 +0900

    外部から取得したプロファイルの一覧を表示するためのViewControllerを追加（中身はこれから）

---
+++ ApnAssister/ViewControllers/DownloadProfileListViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  DownloadProfileListViewCell.swift
+//  ApnAssister
+//
+//  Created by WataruSuzuki on 2016/04/05.
+//  Copyright © 2016年 WataruSuzuki. All rights reserved.
+//
+
+import UIKit
+
+class DownloadProfileListViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

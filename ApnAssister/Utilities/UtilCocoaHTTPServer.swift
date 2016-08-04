5705c33fdf529b7f58f8977731082c51f186963f
WataruSuzuki <wataru0406@gmail.com>
2016-08-04 21:24:50 +0900

    CocoaHTTPServer用のユーティリティーファイルを追加（中身はこれから）

---
+++ ApnAssister/Utilities/UtilCocoaHTTPServer.swift
@@ -0,0 +1,13 @@
+//
+//  UtilCocoaHTTPServer.swift
+//  ApnAssister
+//
+//  Created by WataruSuzuki on 2016/08/04.
+//  Copyright © 2016年 WataruSuzuki. All rights reserved.
+//
+
+import UIKit
+
+class UtilCocoaHTTPServer: NSObject {
+
+}

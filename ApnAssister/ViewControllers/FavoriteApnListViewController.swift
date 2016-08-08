71e09c19c80507685fa9a67bd6ffd66ca07e8ebe
WataruSuzuki <wataru0406@gmail.com>
2016-08-08 12:21:00 +0900

    お気に入り一覧で削除時にクラッシュする問題の解消

--- ApnAssister/ViewControllers/FavoriteApnListViewController.swift
+++ ApnAssister/ViewControllers/FavoriteApnListViewController.swift
@@ -40,17 +40,19 @@ class FavoriteApnListViewController: ApnListViewController {
     }
     */
 
-    /*
     // Override to support editing the table view.
     override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
         if editingStyle == .Delete {
+            let apnSummary = allFavoriteApnSummaryObjs.objectAtIndex(UInt(indexPath.row)) as! ApnSummaryObject
+            myUtilHandleRLMObject.deleteApnSummaryObj(apnSummary)
+            
             // Delete the row from the data source
             tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+            
         } else if editingStyle == .Insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        }
     }
-    */
 
     /*
     // Override to support rearranging the table view.

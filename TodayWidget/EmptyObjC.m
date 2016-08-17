17acaf78bd2398fc59ac8690e0670ab899d54858
WataruSuzuki <wataru0406@gmail.com>
2016-08-17 09:23:31 +0900

    不要なファイルを削除

--- TodayWidget/EmptyObjC.m
+++
@@ -1,9 +0,0 @@
-//
-//  EmptyObjC.m
-//  ApnAssister
-//
-//  Created by WataruSuzuki on 2016/08/17.
-//  Copyright © 2016年 WataruSuzuki. All rights reserved.
-//
-
-#import <Foundation/Foundation.h>

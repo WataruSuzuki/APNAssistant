6e15daea09151756dc2bc84a63e9611bf245e6aa
WataruSuzuki <wataru0406@gmail.com>
2016-08-18 10:35:53 +0900

    UISegmentedControlをのっけたUITableViewCellを追加（中身はこれから）

---
+++ ApnAssister/Views/SegmentedCtrlCell.swift
@@ -0,0 +1,24 @@
+//
+//  SegmentedCtrlCell.swift
+//  ApnAssister
+//
+//  Created by WataruSuzuki on 2016/08/18.
+//  Copyright © 2016年 WataruSuzuki. All rights reserved.
+//
+
+import UIKit
+
+class SegmentedCtrlCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}

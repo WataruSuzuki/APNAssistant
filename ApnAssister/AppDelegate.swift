f6226cd09afbdf33c5d12192a45929b6375798ad
WataruSuzuki <wataru0406@gmail.com>
2016-08-18 14:24:26 +0900

    loadTabBarTitleを呼び出すタイミングをdidFinishLaunchingWithOptionsへ戻す

--- ApnAssister/AppDelegate.swift
+++ ApnAssister/AppDelegate.swift
@@ -20,6 +20,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         UtilHandleRLMObject.copyToGroupDB()
         UtilHandleRLMObject.setupGroupDB()
+        loadTabBarTitle()
         return true
     }
 
@@ -35,7 +36,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillEnterForeground(application: UIApplication) {
         // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-        loadTabBarTitle()
     }
 
     func applicationDidBecomeActive(application: UIApplication) {

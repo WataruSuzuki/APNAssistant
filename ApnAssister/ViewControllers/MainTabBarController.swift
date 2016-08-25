9c925d7a71eec7e314de130f93911241316c44ec
WataruSuzuki <wataru0406@gmail.com>
2016-08-25 11:54:21 +0900

    Main用のtabbarcontrollerを操作するためのクラスを追加（中身はこれから）

---
+++ ApnAssister/ViewControllers/MainTabBarController.swift
@@ -0,0 +1,35 @@
+//
+//  MainTabBarController.swift
+//  ApnAssister
+//
+//  Created by H3032570 on 2016/08/25.
+//  Copyright © 2016年 WataruSuzuki. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

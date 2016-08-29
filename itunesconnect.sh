f6cd0afe4819bebab1861bb62d8ec52cd40478df
WataruSuzuki <wataru0406@gmail.com>
2016-08-29 11:21:43 +0900

    xcodebuild用スクリプトファイルの追加（使うかは微妙だけど）

---
+++ itunesconnect.sh
@@ -0,0 +1,31 @@
+PROJ_FILE_PATH="ApnAssister.xcodeproj"
+TARGET_HOST="ApnAssister"
+OUT_ARCHIVES_DIR="out_archives"
+OUT_IPA_DIR="out_ipa"
+ALTTOOL="/Applications/Xcode.app/Contents/Applications/Application Loader.app/Contents/Frameworks/ITunesSoftwareService.framework/Support/altool"
+ITUNES_ID=""
+ITUNES_PW=""
+
+# 出力先ipaディレクトリ作成
+# -------------------------
+if [ ! -d ${OUT_ARCHIVES_DIR} ]
+then
+    mkdir "${OUT_ARCHIVES_DIR}"
+fi
+if [ ! -d ${OUT_IPA_DIR} ]
+then
+    mkdir "${OUT_IPA_DIR}"
+fi
+
+# 関数の定義
+# -------------------------
+itunes_connect () {
+    xcodebuild clean -project "${PROJ_FILE_PATH}"
+    xcodebuild archive -project "${PROJ_FILE_PATH}" -scheme "${TARGET_HOST}" -archivePath "${PWD}/${OUT_ARCHIVES_DIR}/$1.xcarchive" -configuration $1
+    xcodebuild -exportArchive -archivePath "${PWD}/${OUT_ARCHIVES_DIR}/$1.xcarchive" -exportPath "${PWD}/${OUT_IPA_DIR}" -exportOptionsPlist "${PWD}/exportOptions.Plist"
+    "${ALTTOOL}" --validate-app -f "${PWD}/${OUT_IPA_DIR}/${TARGET_HOST}.ipa"  -u ${ITUNES_ID} -p ${ITUNES_PW}
+    "${ALTTOOL}" --upload-app -f "${PWD}/${OUT_IPA_DIR}/${TARGET_HOST}.ipa"  -u ${ITUNES_ID} -p ${ITUNES_PW}
+}
+
+#Release
+itunes_connect Release

fc1be2ec060973612768477d970c8ff11f8b1408
WataruSuzuki <wataru0406@gmail.com>
2016-09-12 15:51:24 +0900

    profileダウンロードのユーティリティclassを追加（中身はこれから）

---
+++ ApnAssister/Utilities/UtilDownloadProfileList.swift
@@ -0,0 +1,13 @@
+//
+//  UtilDownloadProfileList.swift
+//  ApnAssister
+//
+//  Created by WataruSuzuki on 2016/09/12.
+//  Copyright © 2016年 WataruSuzuki. All rights reserved.
+//
+
+import UIKit
+
+class UtilDownloadProfileList: NSObject {
+
+}

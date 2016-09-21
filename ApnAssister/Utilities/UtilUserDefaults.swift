d12d31f070790615406d0e72e6509c4e50f1097b
WataruSuzuki <wataru0406@gmail.com>
2016-09-22 05:47:52 +0900

    NSUserDefaults用のユーティリティクラスを追加（中身はこれから）

---
+++ ApnAssister/Utilities/UtilUserDefaults.swift
@@ -0,0 +1,13 @@
+//
+//  UtilUserDefaults.swift
+//  ApnAssister
+//
+//  Created by WataruSuzuki on 2016/09/22.
+//  Copyright © 2016年 WataruSuzuki. All rights reserved.
+//
+
+import UIKit
+
+class UtilUserDefaults: NSUserDefaults {
+
+}

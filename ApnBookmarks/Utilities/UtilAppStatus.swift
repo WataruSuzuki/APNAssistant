c0a86c250b5f013eeea5ee725c4e6bddf2ef040b
WataruSuzuki <wataru0406@gmail.com>
2016-09-24 13:35:53 +0900

    アプリの状態をチェックするユーティリティを追加

---
+++ ApnBookmarks/Utilities/UtilAppStatus.swift
@@ -0,0 +1,13 @@
+//
+//  UtilAppStatus.swift
+//  ApnBookmarks
+//
+//  Created by 鈴木 航 on 2016/09/24.
+//  Copyright © 2016年 WataruSuzuki. All rights reserved.
+//
+
+import UIKit
+
+class UtilAppStatus: NSObject {
+
+}

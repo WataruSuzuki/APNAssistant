8dcb8e9ea7213df763a2390f377718684766b909
WataruSuzuki <wataru0406@gmail.com>
2016-09-28 17:57:49 +0900

    AlertやSheetをまとめるクラスを追加（中身はこれから）

---
+++ ApnAssister/Utilities/UtilAlertSheet.swift
@@ -0,0 +1,13 @@
+//
+//  UtilAlertSheet.swift
+//  ApnAssister
+//
+//  Created by WataruSuzuki on 2016/09/28.
+//  Copyright © 2016年 WataruSuzuki. All rights reserved.
+//
+
+import UIKit
+
+class UtilAlertSheet: NSObject {
+
+}

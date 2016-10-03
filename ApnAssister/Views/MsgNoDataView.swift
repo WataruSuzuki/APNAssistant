f2abf77fc9b4bea0d7722408c8eec1fadd412298
WataruSuzuki <wataru0406@gmail.com>
2016-10-03 15:27:57 +0900

    データ無し時にTableViewに貼り付けるViewを作成（中身はこれから）

---
+++ ApnAssister/Views/MsgNoDataView.swift
@@ -0,0 +1,21 @@
+//
+//  MsgNoDataView.swift
+//  ApnAssister2
+//
+//  Created by WataruSuzuki on 2016/10/03.
+//  Copyright © 2016年 WataruSuzuki. All rights reserved.
+//
+
+import UIKit
+
+class MsgNoDataView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}

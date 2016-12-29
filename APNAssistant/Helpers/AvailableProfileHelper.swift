a18e55864618b3520cbb31c6743b2da1c43fb6b2
WataruSuzuki <wataru0406@gmail.com>
2016-12-29 21:43:24 +0900

    プロファイルをキャッシュする処理のカウンター制御に謝りがあったので修正

--- APNAssistant/Helpers/AvailableProfileHelper.swift
+++ APNAssistant/Helpers/AvailableProfileHelper.swift
@@ -29,7 +29,7 @@ class AvailableProfileHelper: NSObject {
     }
     
     func startDownloadAvailableProfiles() {
-        cacheCounter = targetProfileList.count
+        //cacheCounter = targetProfileList.count
         for countries in targetProfileList {
             for country in countries {
                 DispatchQueue.global(qos: .default).async(execute: {
@@ -48,6 +48,7 @@ class AvailableProfileHelper: NSObject {
     func executeDownloadProfile(reqUrl: URL, success:((String) -> Void)?, fail:((Error?) -> Void)?) {
         let config = URLSessionConfiguration.default
         let session = Foundation.URLSession(configuration: config)
+        self.cacheCounter += 1
         let task = session.downloadTask(with: reqUrl, completionHandler: { (location, response, error) in
             self.cacheCounter -= 1
             if let thisResponse = response, let thisLocation = location {

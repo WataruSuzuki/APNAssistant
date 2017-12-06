85e4cbca847eb56b447dd39c1a41d56e5ea9ac01
WataruSuzuki <wataru0406@gmail.com>
2017-12-06 21:30:00 +0900

    Added new custom table view cell template

---
+++ APNAssistant/Views/PickerCell.swift
@@ -0,0 +1,24 @@
+//
+//  PickerCell.swift
+//  APNAssistant
+//
+//  Created by WataruSuzuki on 2017/12/07.
+//  Copyright © 2017年 WataruSuzuki. All rights reserved.
+//
+
+import UIKit
+
+class PickerCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}

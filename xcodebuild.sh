906eba6483245f12cdad5051b05bfef0ee35d457
WataruSuzuki <wataru0406@gmail.com>
2017-12-27 23:00:00 +0900

    Modified travis yml : separated xcodebuild scripts

---
+++ xcodebuild.sh
@@ -0,0 +1,5 @@
+xcodebuild test -workspace APNAssistant.xcworkspace -scheme APNAssistant -sdk iphonesimulator \
+    -destination 'platform=iOS Simulator,name=iPhone 5s' \
+    -destination 'platform=iOS Simulator,name=iPhone 6 Plus' \
+    -destination 'platform=iOS Simulator,name=iPhone 7' \
+    -destination 'platform=iOS Simulator,name=iPhone X' \

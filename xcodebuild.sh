63c02862e4dc6e254bb8088084d214b39824ac52
WataruSuzuki <wataru0406@gmail.com>
2018-01-15 21:30:00 +0900

    Updated xcodebuild script

--- xcodebuild.sh
+++ xcodebuild.sh
@@ -1,7 +1,11 @@
 xcodebuild test -workspace APNAssistant.xcworkspace -scheme APNAssistant -sdk iphonesimulator \
     -destination 'platform=iOS Simulator,name=iPhone 5s,OS=9.3' \
     -destination 'platform=iOS Simulator,name=iPhone 6,OS=10.3.1' \
+
+xcodebuild test -workspace APNAssistant.xcworkspace -scheme APNAssistant -sdk iphonesimulator \
     -destination 'platform=iOS Simulator,name=iPhone 7 Plus' \
     -destination 'platform=iOS Simulator,name=iPhone X,OS=11.2' \
+
+xcodebuild test -workspace APNAssistant.xcworkspace -scheme APNAssistant -sdk iphonesimulator \
     -destination 'platform=iOS Simulator,name=iPad Air 2,OS=10.3.1' \
     -destination 'platform=iOS Simulator,name=iPad Pro (10.5-inch),OS=11.2' \

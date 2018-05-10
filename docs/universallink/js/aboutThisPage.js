4511afb8d378e066db2911ce585a47f341cacbe7
WataruSuzuki <wataru0406@gmail.com>
2018-05-10 22:10:00 +0900

    Added docs from gh-pages branch

---
+++ docs/universallink/js/aboutThisPage.js
@@ -0,0 +1,8 @@
+function launchApp(){
+    var url = "jchankchanapnassistant://";
+    var ref = window.open(url, '_blank', 'location=yes');
+}
+
+function loadProjectAPNAssistant(){
+    var ref = window.open('https://watarusuzuki.github.io/APNAssistant/');
+}

b3ee018803f351f50550927da3559fe163ed4e5f
WataruSuzuki <wataru0406@gmail.com>
2018-05-21 22:10:00 +0900

    Fixed test but not completed yet...

--- APNAssistantUITests/APNAssistantUITests.swift
+++ APNAssistantUITests/APNAssistantUITests.swift
@@ -37,71 +37,68 @@ class APNAssistantUITests: XCTestCase {
         if #available(iOS 9.0, *) {
             let app = XCUIApplication()
             createSimpleAPN(app: app)
-            //createFullAPN(app: app)            
+            createFullAPN(app: app)
         }
     }
 
     @available(iOS 9.0, *)
     private func createFullAPN(app: XCUIApplication) {
-        if UIDevice.current.userInterfaceIdiom == .pad {
-            XCUIApplication()/*@START_MENU_TOKEN@*/.otherElements["PopoverDismissRegion"]/*[[".otherElements[\"dismiss popup\"]",".otherElements[\"PopoverDismissRegion\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
-        } else {
-            app.sheets[UITestUtils.getTestStr(key: "confirm", sender: APNAssistantUITests.self)].buttons[UITestUtils.getTestStr(key: "cancel", sender: APNAssistantUITests.self)].tap()
-        }
+        cancelAvailableList(app)
         tapAddNewProfile(app)
 
         let tablesQuery = app.tables
-        tablesQuery/*@START_MENU_TOKEN@*/.staticTexts["Default apn name"]/*[[".cells.staticTexts[\"Default apn name\"]",".staticTexts[\"Default apn name\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.swipeUp()
+        let summaryTextField = tablesQuery.children(matching: .cell).element(boundBy: 0).children(matching: .textField).element
+        summaryTextField.typeText("Full APN Tests")
+
+        let nextButton = app.buttons["Next:"]
+        nextButton.tap()
+        
+        app.tables.staticTexts[UITestUtils.getTestStr(key: "keyAttachApnName", sender: APNAssistantUITests.self)].tap()
+        let apnCellsQuery = tablesQuery.cells.containing(.staticText, identifier:UITestUtils.getTestStr(key: "keyAttachApnName", sender: APNAssistantUITests.self))
+        
+        let apnNameTextField = apnCellsQuery.children(matching: .textField).element
+        apnNameTextField.tap()
+        apnNameTextField.typeText("full.apnassistant.com")
+        
+        let setDataApnManuallySwitch = tablesQuery/*@START_MENU_TOKEN@*/.switches[UITestUtils.getTestStr(key: "setDataApnManual", sender: APNAssistantUITests.self)]/*[[".cells.switches[\"Set Data Apn manually\"]",".switches[\"Set Data Apn manually\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        setDataApnManuallySwitch/*@START_MENU_TOKEN@*/.tap()/*[[".tap()",".press(forDuration: 1.4);"],[[[-1,1],[-1,0]]],[1]]@END_MENU_TOKEN@*/
+        
+        tablesQuery/*@START_MENU_TOKEN@*/.staticTexts[UITestUtils.getTestStr(key: "keyAttachApnName", sender: APNAssistantUITests.self)]/*[[".cells.staticTexts[\"Default apn name\"]",".staticTexts[\"Default apn name\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.swipeUp()
         app.children(matching: .window).element(boundBy: 0).children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .table).element.swipeUp()
         
+        let dataApnNameCellsQuery = tablesQuery.cells.containing(.staticText, identifier:UITestUtils.getTestStr(key: "keyApnsName", sender: APNAssistantUITests.self))
+        let dataApnNameTextField = dataApnNameCellsQuery.children(matching: .textField).element
+        dataApnNameTextField.tap()
+        dataApnNameTextField.typeText("full.data.apn.com")
         
-        let setDataApnManuallySwitch = tablesQuery/*@START_MENU_TOKEN@*/.switches["Set Data Apn manually"]/*[[".cells.switches[\"Set Data Apn manually\"]",".switches[\"Set Data Apn manually\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
-        setDataApnManuallySwitch/*@START_MENU_TOKEN@*/.press(forDuration: 1.4);/*[[".tap()",".press(forDuration: 1.4);"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        let dataApnUserNameCellsQuery = tablesQuery.cells.containing(.staticText, identifier:UITestUtils.getTestStr(key: "keyApnsUsername", sender: APNAssistantUITests.self))
+        let dataApnUserNameTextField = dataApnUserNameCellsQuery.children(matching: .textField).element
+        dataApnUserNameTextField.tap()
+        dataApnUserNameTextField.typeText("username")
         
-        tablesQuery/*@START_MENU_TOKEN@*/.switches["Set Data Apn manually"].press(forDuration: 0.9);/*[[".cells.switches[\"Set Data Apn manually\"]",".tap()",".press(forDuration: 0.9);",".switches[\"Set Data Apn manually\"]"],[[[-1,3,1],[-1,0,1]],[[-1,2],[-1,1]]],[0,0]]@END_MENU_TOKEN@*/
+//        let dataApnPasswordCellsQuery = tablesQuery.cells.containing(.staticText, identifier:UITestUtils.getTestStr(key: "keyApnsPassword", sender: APNAssistantUITests.self))
+//        let dataApnPasswordTextField = dataApnPasswordCellsQuery.children(matching: .textField).element
+//        dataApnPasswordTextField.tap()
+//        dataApnPasswordTextField.typeText("password")
         
-        let dataApnNameCellsQuery = tablesQuery.cells.containing(.staticText, identifier:"Data apn name")
-        dataApnNameCellsQuery.textFields["Empty"].tap()
-        dataApnNameCellsQuery.children(matching: .textField).element.typeText("full.apn.com")
-        tablesQuery/*@START_MENU_TOKEN@*/.staticTexts["Set Data Apn manually"]/*[[".cells.staticTexts[\"Set Data Apn manually\"]",".staticTexts[\"Set Data Apn manually\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.swipeUp()
-        tablesQuery/*@START_MENU_TOKEN@*/.staticTexts["Data apn user name"]/*[[".cells.staticTexts[\"Data apn user name\"]",".staticTexts[\"Data apn user name\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        tablesQuery.cells.containing(.staticText, identifier:UITestUtils.getTestStr(key: "keyApnsAllowedProtocolMask", sender: APNAssistantUITests.self)).textFields["nothing"].tap()
         
-        let dataApnUserNameCellsQuery = tablesQuery.cells.containing(.staticText, identifier:"Data apn user name")
-        let emptyTextField = dataApnUserNameCellsQuery.textFields["Empty"]
-        emptyTextField.tap()
-        emptyTextField.tap()
-        dataApnUserNameCellsQuery.children(matching: .textField).element.typeText("username")
+//        let nothingPickerWheel = tablesQuery/*@START_MENU_TOKEN@*/.pickerWheels["nothing"]/*[[".cells",".pickers.pickerWheels[\"nothing\"]",".pickerWheels[\"nothing\"]"],[[[-1,2],[-1,1],[-1,0,1]],[[-1,2],[-1,1]]],[0]]@END_MENU_TOKEN@*/
+//        nothingPickerWheel.swipeUp()
+//        nothingPickerWheel.swipeUp()
+//        tablesQuery/*@START_MENU_TOKEN@*/.staticTexts[UITestUtils.getTestStr(key: "keyApnsAllowedProtocolMask", sender: APNAssistantUITests.self)]/*[[".cells.staticTexts[\"Data apn protocol\"]",".staticTexts[\"Data apn protocol\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
         
-        let dataApnPasswordCellsQuery = tablesQuery.cells.containing(.staticText, identifier:"Data apn password")
-        let emptySecureTextField = dataApnPasswordCellsQuery.secureTextFields["Empty"]
-        emptySecureTextField.tap()
-        emptySecureTextField.tap()
-        dataApnPasswordCellsQuery.children(matching: .secureTextField).element.typeText("password")
-        tablesQuery/*@START_MENU_TOKEN@*/.staticTexts["Data apn password"]/*[[".cells.staticTexts[\"Data apn password\"]",".staticTexts[\"Data apn password\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.swipeUp()
-        tablesQuery.cells.containing(.staticText, identifier:"Data apn protocol").textFields["nothing"].tap()
-        
-        let nothingPickerWheel = tablesQuery/*@START_MENU_TOKEN@*/.pickerWheels["nothing"]/*[[".cells",".pickers.pickerWheels[\"nothing\"]",".pickerWheels[\"nothing\"]"],[[[-1,2],[-1,1],[-1,0,1]],[[-1,2],[-1,1]]],[0]]@END_MENU_TOKEN@*/
-        nothingPickerWheel.swipeUp()
-        nothingPickerWheel.swipeUp()
-        tablesQuery/*@START_MENU_TOKEN@*/.staticTexts["Data apn protocol"]/*[[".cells.staticTexts[\"Data apn protocol\"]",".staticTexts[\"Data apn protocol\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
-        app.navigationBars["Edit APN"].buttons["Save"].tap()
-        app.sheets["Is update setting now?"].buttons["Not this time"].tap()
-        
+        saveNewProfile(app)
     }
 
     @available(iOS 9.0, *)
     private func createSimpleAPN(app: XCUIApplication) {
-        
-        if UIDevice.current.userInterfaceIdiom == .pad {
-            XCUIApplication()/*@START_MENU_TOKEN@*/.otherElements["PopoverDismissRegion"]/*[[".otherElements[\"dismiss popup\"]",".otherElements[\"PopoverDismissRegion\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
-        } else {
-            app.sheets[UITestUtils.getTestStr(key: "confirm", sender: APNAssistantUITests.self)].buttons[UITestUtils.getTestStr(key: "cancel", sender: APNAssistantUITests.self)].tap()
-        }
+        cancelAvailableList(app)
         tapAddNewProfile(app)
         
         let tablesQuery = app.tables
         let textField = tablesQuery.children(matching: .cell).element(boundBy: 0).children(matching: .textField).element
-        textField.typeText("Simple UI Tests")
+        textField.typeText("Simple APN Tests")
         
         let nextButton = app.buttons["Next:"]
         nextButton.tap()
@@ -113,6 +110,20 @@ class APNAssistantUITests: XCTestCase {
         textField2.tap()
         textField2.typeText("simple.apnassistant.com")
         
+        saveNewProfile(app)
+    }
+    
+    @available(iOS 9.0, *)
+    fileprivate func cancelAvailableList(_ app: XCUIApplication) {
+        if UIDevice.current.userInterfaceIdiom == .pad {
+            XCUIApplication().tap()
+        } else {
+            app.sheets[UITestUtils.getTestStr(key: "confirm", sender: APNAssistantUITests.self)].buttons[UITestUtils.getTestStr(key: "cancel", sender: APNAssistantUITests.self)].tap()
+        }
+    }
+    
+    @available(iOS 9.0, *)
+    fileprivate func saveNewProfile(_ app: XCUIApplication) {
         app.navigationBars[UITestUtils.getTestStr(key: "edit_apn", sender: APNAssistantUITests.self)].buttons[(UITestUtils.isJapanese(sender: APNAssistantUITests.self) ? "保存" : "Save")].tap()
         if UIDevice.current.userInterfaceIdiom == .pad {
             XCUIApplication()/*@START_MENU_TOKEN@*/.otherElements["PopoverDismissRegion"]/*[[".otherElements[\"dismiss popup\"]",".otherElements[\"PopoverDismissRegion\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
@@ -120,8 +131,7 @@ class APNAssistantUITests: XCTestCase {
             app.sheets[UITestUtils.getTestStr(key: "is_update_now", sender: APNAssistantUITests.self)].buttons[UITestUtils.getTestStr(key: "not_this_time", sender: APNAssistantUITests.self)].tap()
         }
     }
-    
-    
+
     @available(iOS 9.0, *)
     fileprivate func tapAddNewProfile(_ app: XCUIApplication) {
         app.tabBars.buttons[UITestUtils.getTestStr(key: "profileList", sender: APNAssistantUITests.self)].tap()
